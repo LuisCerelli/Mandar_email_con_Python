e9234ae4e88a0e3bd66859c899b3167dc57df224
LuisCerelli <cerluisalberto@gmail.com>
2024-09-26 14:05:34 +0200

    update

--- mandar_email.py
+++ mandar_email.py
@@ -3,3 +3,24 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 your_email = 'degiovanniatilio@gmail.com'
+your_password = 'egtj qdod mieu qxnl'
+
+recipent = 'cerluisalberto@gmail.com'
+
+message = MIMEMultipart()
+message['From'] = your_email
+message['To'] = recipent
+message['Subject'] = 'Email de agradecimiento'
+
+body = 'Muchas gracias a todos por el apoyo! Se agradece mucho porque cada vez somos mas!!!'
+
+message.attach(MIMEText(body, 'plain'))
+
+smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
+smtp_server.starttls()
+smtp_server.login(your_email, your_password)
+
+smtp_server.sendmail(your_email, recipent, message.as_string())
+smtp_server.quit
+print('Email enviado')
+
